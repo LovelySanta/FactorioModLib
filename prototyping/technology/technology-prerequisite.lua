c7fd04052d079ec36d78459da7b48b6212a8d705
LovelySanta <laurens.scheldeman@gmail.com>
2019-05-07 23:17:13 +0200

    Fixed long loading times on the techTreeCleanup

--- prototyping/technology/technology-prerequisite.lua
+++ prototyping/technology/technology-prerequisite.lua
@@ -54,73 +54,110 @@ if not LSlib.technology then require "technology" else
 
 
   function LSlib.technology.getAllPrerequisites(technologyName)
-    if not data.raw["technology"][technologyName] then return nil end
-    if not data.raw["technology"][technologyName].prerequisites then return nil end
+    if not data.raw["technology"][technologyName]               then return end
+    if not data.raw["technology"][technologyName].prerequisites then return end
 
-    if LSlib.technology.prereqcache[technologyName] then return LSlib.technology.prereqcache[technologyName] end
+    if LSlib.technology.prereqcache[technologyName] then
+      return LSlib.technology.prereqcache[technologyName]
+    else
+      -- temporary fill in the prerequisites to prevent stack overflow due to loops in technology tree
+      LSlib.technology.prereqcache[technologyName] = {}
 
-    -- temporary till we find all prerequisites (this is to prevent loop, stack overflow)
-    LSlib.technology.prereqcache[technologyName] = util.table.deepcopy(data.raw["technology"][technologyName].prerequisites)
+      local prerequisites = {} -- all prerequisites for this tech
+      for _, prerequisiteName in pairs(data.raw["technology"][technologyName].prerequisites) do
+        LSlib.technology.prereqcache[technologyName][prerequisiteName] = true
 
-    -- get all none-reduntant prerequisites now
-    local prerequisites = util.table.deepcopy(data.raw["technology"][technologyName].prerequisites)
-    for _,prerequisiteName in pairs(data.raw["technology"][technologyName].prerequisites) do
-      local prerequisiteTable = LSlib.technology.getAllPrerequisites(prerequisiteName)
-      if prerequisiteTable then
-        for _, prerequisite in pairs(prerequisiteTable) do
-          table.insert(prerequisites, prerequisite)
+        -- now get the prerequisite of all the prerequisites and add those as well
+        local prePrerequisites = LSlib.technology.getAllPrerequisites(prerequisiteName)
+        for prePrerequisiteName,_ in pairs(prePrerequisites or {}) do
+          LSlib.technology.prereqcache[technologyName][prePrerequisiteName] = true
         end
+      end
+
+      -- now we have all prerequisites
+      return LSlib.technology.prereqcache[technologyName]
+    end
+  end
+
+
+
+  function LSlib.technology.removeDuplicatePrerequisites(technologyName)
+    if not data.raw["technology"][technologyName]               then return end
+    if not data.raw["technology"][technologyName].prerequisites then return end
+
+    -- STEP 1: obtain all the prerequisites once (so no duplicates)
+    local prerequisites = {}
+    local containDuplicates = false
+    for _,prerequisiteName in pairs(data.raw["technology"][technologyName].prerequisites) do
+      if prerequisites[prerequisiteName] then -- already contains one, we found a duplicate
+        containDuplicates = true
+        LSlib.utils.log.log(string.format("TechTreeCleanup: Removing duplicate prerequisite %q from technology %q.", prerequisiteName, technologyName))
+      else
+        prerequisites[prerequisiteName] = true
       end
     end
 
-    -- save all the none-redundant prerequisites now
-    LSlib.technology.prereqcache[technologyName] = prerequisites
-    return prerequisites
+
+    if containDuplicates then
+      -- STEP 2: if we have duplicates, we create a new prerequisite table without duplicates
+      local newPrerequisites = {}
+      for prerequisiteName,_ in pairs(prerequisites) do
+        table.insert(newPrerequisites, prerequisiteName)
+      end
+
+      -- STEP 3: we use this newly created prerequisite table in the technology tree
+      data.raw["technology"][technologyName].prerequisites = util.table.deepcopy(newPrerequisites)
+    end
   end
 
 
 
   function LSlib.technology.removeRedundantPrerequisites(technologyName)
+    if not data.raw["technology"][technologyName]               then return end
     if not data.raw["technology"][technologyName].prerequisites then return end
 
-    -- iterate over all prerequisites to check if they are redundant
-    for _,prerequisiteName in pairs(util.table.deepcopy(data.raw["technology"][technologyName].prerequisites)) do
+    -- we will check for all prerequisites
+    local oldPrerequisites = util.table.deepcopy(data.raw["technology"][technologyName].prerequisites) -- make it read only
+    local removedPrerequisites = {}
+    for _,prerequisiteName in pairs(oldPrerequisites) do
 
       -- STEP 1: obtain the prerequisites of a prerequisite
-      local prerequisites = LSlib.technology.getAllPrerequisites(prerequisiteName)
-      if prerequisites then
+      local prePrerequisites = LSlib.technology.getAllPrerequisites(prerequisiteName) or {}
 
-        -- STEP 2a: check for all the other prerequisites of the technology
-        for _,prerequisite in pairs(util.table.deepcopy(data.raw["technology"][technologyName].prerequisites)) do
-          if prerequisite ~= prerequisiteName then -- no need to check itself
+      -- STEP 2a: now we check if any prerequisite of the tech is redundant because
+      --          it could be a prerequisite of this prerequisite
+      for redundantPrerequisiteIndex,redundantPrerequisiteName in pairs(oldPrerequisites) do
+        if redundantPrerequisiteName ~= prerequisiteName         and -- no need to check itself
+           (not removedPrerequisites[redundantPrerequisiteName]) and -- not already deleted
+           prePrerequisites[redundantPrerequisiteName]           then -- must be redundant
 
-            -- STEP 2b: remove the technology prerequisite if the prerequisite is a prerequisite of another technology prerequisite
-            for _, name in pairs(prerequisites) do
-              if name == prerequisite then
-                --remove prerequisite
-                for k,v in pairs(data.raw["technology"][technologyName].prerequisites) do
-                  if v == prerequisite then
-                    table.remove(data.raw["technology"][technologyName].prerequisites, k)
-                    LSlib.utils.log.log(string.format("TechTreeCleanup: Removing prerequisite %q from technology %q.", prerequisite, technologyName))
-                  end
-                end
-              end
-            end
-          end
+          -- STEP 2b: remove the technology prerequisite if the prerequisite is a prerequisite of another technology prerequisite
+          data.raw["technology"][technologyName].prerequisites[redundantPrerequisiteIndex] = nil -- remove it
+          removedPrerequisites[redundantPrerequisiteName] = true
+          LSlib.utils.log.log(string.format("TechTreeCleanup: Removing redundant prerequisite %q from technology %q.", redundantPrerequisiteName, technologyName))
         end
       end
+
     end
   end
 
 
 
   function LSlib.technology.removeAllRedundantPrerequisites()
+    --LSlib.utils.log.enable()
+
     if not LSlib.technology.prereqcache then
       LSlib.technology.prereqcache = {}
     end
 
-    for _, technology in pairs(data.raw["technology"]) do
-      LSlib.technology.removeRedundantPrerequisites(technology.name)
+    -- STEP 1: we remove duplicate entries in the technology table
+    for technologyName, technology in pairs(data.raw["technology"]) do
+      LSlib.technology.removeDuplicatePrerequisites(technologyName)
+    end
+
+
+    for technologyName, technology in pairs(data.raw["technology"]) do
+      LSlib.technology.removeRedundantPrerequisites(technologyName)
     end
 
     LSlib.technology.prereqcache = nil
