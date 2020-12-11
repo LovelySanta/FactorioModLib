85f8dcd27061008ac95b7c8af6ea0ca778ee63aa
AlienXAXS <mark.phillips@axsemail.co.uk>
2020-12-11 18:31:33 +0000

    Fixes in LSlib.recipe namespace. (#7)

--- LSlib/prototyping/recipe/recipe-result.lua
+++ LSlib/prototyping/recipe/recipe-result.lua
@@ -200,7 +200,8 @@ if not LSlib.recipe then require "recipe" else
     elseif data.raw["recipe"][recipeName].results then
       for resultIndex, result in pairs(data.raw["recipe"][recipeName].results) do
         if resultName == nil or (result.name or result[1] or "") == resultName then
-          return data.raw["recipe"][recipeName].results[resultIndex].amount or 1
+          return data.raw["recipe"][recipeName].results[resultIndex].amount or
+                 data.raw["recipe"][recipeName].results[resultIndex][2] or 1
         end
       end
       return 0
@@ -215,7 +216,8 @@ if not LSlib.recipe then require "recipe" else
       elseif data.raw["recipe"][recipeName].normal.results then
         for resultIndex, result in pairs(data.raw["recipe"][recipeName].normal.results) do
           if resultName == nil or (result.name or result[1] or "") == resultName then
-            return data.raw["recipe"][recipeName].results[resultIndex].normal.amount or 1
+            return data.raw["recipe"][recipeName].normal.results[resultIndex].amount or 
+                   data.raw["recipe"][recipeName].normal.results[resultIndex][2] or 1
           end
         end
         return 0
@@ -231,7 +233,8 @@ if not LSlib.recipe then require "recipe" else
       elseif data.raw["recipe"][recipeName].expensive.results then
         for resultIndex, result in pairs(data.raw["recipe"][recipeName].expensive.results) do
           if resultName == nil or (result.name or result[1] or "") == resultName then
-            return data.raw["recipe"][recipeName].results[resultIndex].expensive.amount or 1
+            return data.raw["recipe"][recipeName].expensive.results[resultIndex].amount or
+                   data.raw["recipe"][recipeName].expensive.results[resultIndex][2] or 1
           end
         end
         return 0
@@ -251,10 +254,14 @@ if not LSlib.recipe then require "recipe" else
 
     if data.raw["recipe"][recipeName].results then
       for index, result in pairs(data.raw["recipe"][recipeName].results) do
-        if result.name == resultName then
-          result.amount = resultAmount
-          result.amount_min = nil
-          result.amount_max = nil
+        if (result.name or result[1] or "") == resultName then
+          if result.name then
+            result.amount = resultAmount
+            result.amount_min = nil
+            result.amount_max = nil
+          else
+            result[2] = resultAmount
+          end
           break
         end
       end
