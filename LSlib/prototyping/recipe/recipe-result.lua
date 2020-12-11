526bc1d4827472beedfcb8fa58fa8e55a3e7a165
AlienXAXS <mark.phillips@axsemail.co.uk>
2020-12-11 17:36:04 +0000

    Minor typo fix that would cause a nil value error during data stage

--- LSlib/prototyping/recipe/recipe-result.lua
+++ LSlib/prototyping/recipe/recipe-result.lua
@@ -215,7 +215,7 @@ if not LSlib.recipe then require "recipe" else
       elseif data.raw["recipe"][recipeName].normal.results then
         for resultIndex, result in pairs(data.raw["recipe"][recipeName].normal.results) do
           if resultName == nil or (result.name or result[1] or "") == resultName then
-            return data.raw["recipe"][recipeName].results[resultIndex].normal.amount or 1
+            return data.raw["recipe"][recipeName].normal.results[resultIndex].amount or 1
           end
         end
         return 0
@@ -231,7 +231,7 @@ if not LSlib.recipe then require "recipe" else
       elseif data.raw["recipe"][recipeName].expensive.results then
         for resultIndex, result in pairs(data.raw["recipe"][recipeName].expensive.results) do
           if resultName == nil or (result.name or result[1] or "") == resultName then
-            return data.raw["recipe"][recipeName].results[resultIndex].expensive.amount or 1
+            return data.raw["recipe"][recipeName].expensive.results[resultIndex].amount or 1
           end
         end
         return 0
